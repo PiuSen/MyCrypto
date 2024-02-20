69af2aa1db5d99224f2191ec9f850b89e295854f
Peu Sen <peusen113@gmail.com>
2024-02-20 22:43:27 +0530

    changes

--- app/src/main/java/com/example/mycrypto/Adapter/TopMarketAdapter.kt
+++ app/src/main/java/com/example/mycrypto/Adapter/TopMarketAdapter.kt
@@ -33,7 +33,7 @@ class TopMarketAdapter(var context: Context,val mlist:List<CryptoCurrency>):Recy
         Glide.with(context).load("https://s2.coinmarketcap.com/static/img/coins/64x64/"+item.id+".png")
             .thumbnail(Glide.with(context).load(R.drawable.loading))
             .into(holder.binding.topCurrencyImageView)
-        if(item.quotes!![0].percentChange24h>0){
+        if(item.quotes[0].percentChange24h>0){
             holder.binding.topCurrencyChangeTextView.setTextColor(context.resources.getColor(R.color.green))
             holder.binding.topCurrencyChangeTextView.text="+${item.quotes[0].percentChange24h}%"
 
